de331db162f3e0f4df30b8aeb6a57dfe200fa903
DarylZhang <daryl.zxw@gmail.com>
2021-09-13 21:05:23 +1000

    Delete build directory

--- build/resources/main/abstract_jb.c_get_now.c
+++
@@ -1,9 +0,0 @@
-static long get_now(struct ast_jb *jb, struct timeval *when)
-{
-	struct timeval now;
-	if (!when) {
-		when = &now;
-		gettimeofday(when, NULL);
-	}
-	return ast_tvdiff_ms(*when, jb->timebase);
-}
--- build/resources/main/app_queue.c_queue_hash_cb.c
+++
@@ -1,5 +0,0 @@
-static int queue_hash_cb(const void *obj, const int flags, struct ast_t *t)
-{
-	const struct call_queue *q = obj;
-	return ast_str_case_hash(q->name);
-}
